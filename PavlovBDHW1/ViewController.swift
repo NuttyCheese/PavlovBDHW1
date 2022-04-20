5577df265adfd2002148439e1552744ff41ebec9
BorisPavlov <pbd_b@mail.ru>
2022-04-20 17:52:31 +0300

    Initial Commit

---
+++ PavlovBDHW1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PavlovBDHW1
+//
+//  Created by Борис Павлов on 20.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
